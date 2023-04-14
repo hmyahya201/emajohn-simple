c03b1b52295b1848e68173ec89923bf9e6670319
hmyahya201 <hmyahya001@gmail.com>
2023-04-14 15:36:35 +0600

    commit cart in ui

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -5,16 +5,19 @@ const Cart = ({cart}) => {
     console.log(cart)
     let totalPrice = 0;
     let shipping = 0;
+    let quantity = 0;
     for(const product of cart){
-        totalPrice = totalPrice + product.price
+        // product.quantity = product.quantity || 1;
+        totalPrice = totalPrice + product.price * product.quantity;
         shipping = shipping + product.shipping;
+        quantity = quantity + product.quantity;
     };
     const tax = totalPrice*2/100.
     const grandTotal = totalPrice + shipping + tax;
     return (
         <div className='cart'>
             <h3>Order Summary</h3>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping: ${shipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -15,7 +15,17 @@ const Shop = () => {
   }, []);
 
   const addToCart = (product)=>{
-    const newCart = [...cart, product];
+    let newCart = [];
+    const exist = cart.find(pd=>pd.id === product.id);
+    if(!exist){
+      product.quantity = 1;
+      newCart = [...cart, product];
+    }else{
+      exist.quantity = exist.quantity + 1;
+      const remaining = cart.filter(pd=>pd.id !== product.id);
+      newCart = [...remaining, exist];
+    }
+  
     setCart(newCart)
     addToDb(product.id)
   };
